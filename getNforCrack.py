a19fb0925dd79dd31ac532132b3935182b8db447
Tairoon <tairoon1@gmail.com>
2017-08-03 13:49:48 -0700

    for fine mesh with maximum

--- getNforCrack.py
+++ getNforCrack.py
@@ -1,27 +1,27 @@
 from ovito.io import import_file, export_file
 from ovito.modifiers import *
+import numpy as np
 
 # Define the custom modifier function:
 def modify(frame, input, output):
-    global i
     global N
-    print(input['lambda'].array[i],i,frame)
-    if input['lambda'].array[i]==0:
+    print(np.amax(input['c_3[2]'].array))
+    maxIndex = np.argmax(input['c_3[2]'].array)
+    print(input['lambda'].array[maxIndex],maxIndex,frame)
+    if input['lambda'].array[maxIndex]==0:
         N = N + [frame]
-        i = i + 1
 
 N = []
-i = 0
 
 
 node = import_file('dump_fatigue.peri*')
-node.modifiers.append(SelectExpressionModifier(expression = 'abs(Position.Y)>0.001 || Position.Z>0.016 || Position.Z<0.011 || Position.X<-0.2399'))
+node.modifiers.append(SelectExpressionModifier(expression = 'abs(Position.Y)>0.001 || Position.Z>0.014 || Position.Z<0.012 || Position.X<-0.2399'))
 node.modifiers.append(DeleteSelectedParticlesModifier())
 node.modifiers.append(PythonScriptModifier(function = modify))
 
 for frame in range(node.source.num_frames):
     node.compute(frame)
-    tmp = '[0'
+    tmp = '[1'
     for aaa in N:
         tmp = tmp+' '+str(aaa)
     tmp = tmp + ']'
